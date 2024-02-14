b6898793ecc87cb043b4d3c42bcaf4388d30fafd
Henner Zeller <h.zeller@acm.org>
2024-02-13 22:28:15 -0800

    Add shell.nix for easy set-up of environment

---
+++ shell.nix
@@ -0,0 +1,17 @@
+# This is a nix-shell for use with the nix package manager.
+# If you have nix installed, you may simply run `nix-shell`
+# in this repo, and have all dependencies ready in the new shell.
+
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      kicad
+      pkgsCross.avr.buildPackages.gcc8
+      avrdude
+      python311Packages.kicad
+      python3
+      zip
+      git   # to tag the pcb versions.
+    ];
+}
